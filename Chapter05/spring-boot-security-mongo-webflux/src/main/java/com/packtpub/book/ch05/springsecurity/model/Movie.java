78654351d9148ce8ccf540e23980fa7ffe6ffb77
robert.lee <robert0714@gmail.com>
2018-11-29 15:59:32 +0800

    adjust for lombo dependencies

--- Chapter05/spring-boot-security-mongo-webflux/src/main/java/com/packtpub/book/ch05/springsecurity/model/Movie.java
+++ Chapter05/spring-boot-security-mongo-webflux/src/main/java/com/packtpub/book/ch05/springsecurity/model/Movie.java
@@ -1,18 +1,52 @@
 package com.packtpub.book.ch05.springsecurity.model;
 
 import lombok.*;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "movies")
 public class Movie {
 
-  private Long id;
-  private String title;
-  private String genre;
+	private Long id;
+	private String title;
+	private String genre;
+
+	
+	public Movie(Long id, String title, String genre) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+	}
+	public Movie( ) {
+		super(); 
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
 
 }
--- Chapter05/spring-boot-security-webclient-webflux/src/main/java/com/packtpub/book/ch05/springsecurity/model/Movie.java
+++ Chapter05/spring-boot-security-webclient-webflux/src/main/java/com/packtpub/book/ch05/springsecurity/model/Movie.java
@@ -9,8 +9,44 @@ import lombok.*;
 @AllArgsConstructor
 public class Movie {
 
-  private Long id;
-  private String title;
-  private String genre;
+	private Long id;
+	private String title;
+	private String genre;
+
+	
+	public Movie(Long id, String title, String genre) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+	}
+	public Movie( ) {
+		super(); 
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
 
 }
